6cac946d3e5c0958816ab4924b0369769b81a9e1
Mar1337gh <166358104+Mar1337gh@users.noreply.github.com>
2024-11-30 19:59:40 +0400

    доб комм

--- fend/src/components/ModalBox.js
+++ fend/src/components/ModalBox.js
@@ -3,13 +3,13 @@ import './ModalBox.css';
 
 function ModalBox({setModalBox, children}) {
   return (
-   <>
+   <> 
    <div className='echo' onClick={ () => setModalBox ('none') }> </div>
    <div className="ModalBox">
    {children}
     </div>
 
-  </>
+  </>//поверх всех
   );
 }
 
